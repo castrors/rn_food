33ddd742f21d938e670b81244e262d2bd862589e
Rodrigo Castro <rodrigo.castro@avira.com>
2021-02-24 15:43:36 +0100

    Finishes yelp app

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import ResultsList from '../components/ResultsList';
+import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
+
+const SearchScreen = () => {
+    const [term, setTerm] = useState('');
+    const [searchApi, results, errorMessage] = useResults();
+
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price == price;
+        });
+    };
+
+    return (
+        <>
+            <SearchBar
+                term={term}
+                onTermChange={setTerm}
+                onTermSubmit={() => searchApi(term)}
+            />
+            {!!errorMessage && <Text>{errorMessage}</Text>}
+            <ScrollView>
+                <ResultsList
+                    results={filterResultsByPrice('$')}
+                    title="Cost Effective" />
+                <ResultsList
+                    results={filterResultsByPrice('$$')}
+                    title="Bit Pricier" />
+                <ResultsList
+                    results={filterResultsByPrice('$$$')}
+                    title="Big Spender" />
+                <ResultsList
+                    results={filterResultsByPrice('$$$$')}
+                    title="Show me the money" />
+            </ScrollView>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: 'white',
+        flex: 1
+    }
+});
+
+export default SearchScreen;
